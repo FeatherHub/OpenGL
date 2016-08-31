ce2619c846f88f77358f903200ee1312ce64e7fd
FeatherHub <woook95@naver.com>
2016-08-31 22:27:47 +0900

    Change folder name

---
+++ CG_Practice01_InitialTest/Main.cpp
@@ -0,0 +1,85 @@
+#include <glut.h>
+
+//gl : openGL 함수
+//gl-u : gl의 함수 몇 개를 최적의 성능을 발휘다로고 하나의 함수로 묶음(union)
+//gl-ut : 라이브러리를 각각의 OS에 맞게 최적화
+
+void RenderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	//실제 clear 작업 수행
+
+	glColor3f(0.1f, 0.4f, 0.6f);
+	//드로잉할 색상을 지정한다
+
+	glRectf(0.0f, 0.0f, 40.0f, 40.0f);
+	//x1, y1부터 x2, y2까지 사각형을 그린다
+
+	glFlush();
+	//Queue에 채워진 명령어들을 수행
+	//Rendering pipeline 을 사용하는 OpenGL 명령어들은 종종 Queue에 저장
+	//OpenGL에게 더 이상 명령 받지 말고 지금까지 요청을 처리하도록 지시
+}
+
+void SetupRC()
+{
+	glClearColor(0.3f, 0.6f, 0.3f, 1.0f);
+	//생성된 window를 clear할 색상 설정 - clear 작업 수행은 아직 안 함
+	//OpenGL 구현에서 float로 정의
+	//0.0~1.0 사이의 값으로 정의
+
+	//마지막 component = alpha component
+	//blending, translucence 표현에 사용
+}
+
+void WinSizeChanged(GLsizei w, GLsizei h)
+{
+	glViewport(0, 0, w, h);
+	//윈도우 내의 일정영역을 실제 스크린 좌표로 시정하여 사용한다
+	//OpenGL이 실제로 그림을 그리는데 사용하는 영역
+	//분할 설정 가능(multi-viewport)
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (w <= h)
+		glOrtho(-100.0f, 100.0f, -100.0f * h / w, 100.0f *h / w, 1.0f, -1.0f);	
+		//left right bottom top near far
+	else //w > h
+		glOrtho(-100.0f * w / h, 100.0f*w / h, -100.0f, 100.0f, 1.0f, -1.0f);
+		//직교 투영
+		//2차원 좌표계에서 깊이 성분을 추가하여 3차원으로 확장한다
+		//3차원 Canvas
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void main()
+{
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	//GLUT_SINGLE : Single buffered 
+	//모든 드로잉 명령들을 화면에 나타난 윈도우 상에서 수행
+
+	//GLUT_RGB : RGBA color mode 
+	//1 component : 8 bit
+
+	glutCreateWindow("Practice 01");
+	//Create window using glut API
+
+	glutDisplayFunc(RenderScene);
+	//Display callback function
+	//윈도우를 그릴 때마다 GLUT는 등록된 함수를 호출한다
+
+	glutReshapeFunc(WinSizeChanged);
+	//윈도우 사이즈가 바뀔 때마다 호출할 함수를 등록한다
+
+	SetupRC();
+	//개인이 만든 함수 즉 예약 함수 아님
+	//Rendering 전에 수행될 OpenGL 초기화 작업을 한다
+
+	glutMainLoop();
+	//Include 한 GLUT framework를 호출
+	//메시지, 키보드 입력 등과 같은 운영체제에 관련된 모든 사항을
+	//프로그램이 종료될 때까지 처리한다
+}
---
+++ CG_Practice02_BounceRect/BounceRectangle.cpp
@@ -0,0 +1,114 @@
+#include <glut.h>
+
+//사각형의 크기와 시작 위치
+GLfloat x1 = -10.f;
+GLfloat y1 = -10.f;
+GLsizei rSize = 10.f;
+
+//x와 y 방향으로 움직이는 크기
+GLfloat xStep = 10.f;
+GLfloat yStep = 10.f;
+
+//윈도우 크기
+GLfloat winSizeWidth;
+GLfloat winSizeHeight;
+
+void SetupRC()
+{
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+}
+
+void RenderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(x1, y1, x1/y1);
+
+	glRectf(x1, y1, x1+rSize, y1+rSize);
+	//사각형 정보를 참고하여 사각형을 그린다
+
+	glutSwapBuffers();
+	//드로잉 실행 후 버퍼를 교체한다
+	//glFlush()를 내장하고 있다
+}
+
+void TimerFunc(int val)
+{
+	//방향 변경
+	if (x1 > winSizeWidth - rSize ||
+		x1 < -winSizeWidth)
+		xStep = -xStep;
+
+	if (y1 > winSizeHeight - rSize ||
+		y1 < -winSizeHeight)
+		yStep = -yStep;
+
+	//윈도우 크기가 변경되어 윈도우 화면 경계를 넘어갔을 때
+	if (x1 > winSizeWidth - rSize)
+		x1 = winSizeWidth - rSize - 1;
+	
+	if (y1 > winSizeHeight - rSize)
+		y1 = winSizeHeight - rSize - 1;
+
+	x1 += xStep;
+	y1 += yStep;
+
+	glutPostRedisplay();
+	//현재의 윈도우를 Refresh한다
+	//glutDisplayFunc()를 호출한다
+
+	glutTimerFunc(33, TimerFunc, 1);
+	//33밀리 초 뒤에 등록된 함수를 호출한다 
+}
+
+void ChangeSize(GLsizei w, GLsizei h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (w <= h)
+	{
+		winSizeWidth = 100.f;
+		winSizeHeight = 100.f * h / w;
+
+		glOrtho(-100.f, 100.f, -100.f * h / w, 100.f*h / w, 1.f, -1.f);
+	}
+	else
+	{
+		winSizeWidth = 100.f * w / h;
+		winSizeHeight = 100.f;
+
+		glOrtho(-100.f * w / h, 100.f*w / h, -100.f, 100.f, 1.f, -1.f);
+	}
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void main()
+{
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	//화면에 Draw를 수행하는 동시에 
+	//화면에 나타나지 않는 버퍼에 Render를 수행한다
+	//오래 걸리는 복잡한 그림을 그리며 보여주는 게 아니라
+	//모두 그린 후에 보여준다는 전략이다
+
+	glutInitWindowSize(800, 600);
+
+	glutCreateWindow("Bounce Rectangle");
+	
+	glutDisplayFunc(RenderScene);
+	//그릴 때 마다 호출할 함수를 등록한다
+
+	glutReshapeFunc(ChangeSize);
+	//윈도우 사이즈가 바뀔 때 마다 호출할 함수를 등록한다
+
+	glutTimerFunc(2000, TimerFunc, 1);
+	//msecs : msecs 만큼 기다린 뒤 등록된 함수를 호출한다
+	//val : 사용자 정의 값
+
+	SetupRC();
+
+	glutMainLoop();
+}
